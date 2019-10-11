728431e572b025e6eba16d75879fd630836464dd
Lewis Bell <lewisbell999@gmail.com>
2019-10-11 22:54:33 +0100

    Add all instructions

--- vectorman-assembler/src/main/scala/assembler/InstructionParser.scala
+++ vectorman-assembler/src/main/scala/assembler/InstructionParser.scala
@@ -3,9 +3,10 @@ package assembler
 import scala.util.parsing.combinator.RegexParsers
 
 class InstructionParser extends RegexParsers {
-  def number: Parser[Int] = """[0-9]{1,2}""".r ^^ {
-    _.toInt
-  }
+  def number: Parser[Int] =
+    """[0-9]{1,2}""".r ^^ {
+      _.toInt
+    }
 
   def reg: Parser[Register] = "r" ~> number ^^ {
     identity
@@ -27,7 +28,50 @@ class InstructionParser extends RegexParsers {
     case a ~ b ~ c => Div(List(a, b, c))
   }
 
-  def instruction: Parser[Instruction] = add | sub | mul | div
+  def lod: Parser[Lod] = "LOD" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Lod(List(a, b, c))
+  }
+
+  def str: Parser[Str] = "STR" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Str(List(a, b, c))
+  }
+
+  def bra: Parser[Bra] = "BRA" ~> reg ^^ (a => Bra(List(a)))
+
+  def jmp: Parser[Jmp] = "JMP" ~> reg ^^ (a => Jmp(List(a)))
+
+  def ble: Parser[Ble] = "BLE" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Ble(List(a, b, c))
+  }
+
+  def cmp: Parser[Cmp] = "CMP" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Cmp(List(a, b, c))
+  }
+
+  def and: Parser[And] = "AND" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => And(List(a, b, c))
+  }
+
+  def not: Parser[Not] = "NOT" ~> reg ~ reg ^^ {
+    case a ~ b => Not(List(a, b))
+  }
+
+  def rsh: Parser[Rsh] = "RSH" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Rsh(List(a, b, c))
+  }
+
+  def beq: Parser[Beq] = "BEQ" ~> reg ~ reg ~ reg ^^ {
+    case a ~ b ~ c => Beq(List(a, b, c))
+  }
+
+  def cpy: Parser[Cpy] = "CPY" ~> reg ~ reg ^^ {
+    case a ~ b => Cpy(List(a, b))
+  }
+
+  def instruction: Parser[Instruction] = add | sub | mul | div |
+                                         lod | str | bra | jmp |
+                                         ble | cmp | and | not |
+                                         rsh | beq | cpy
 
   def program: Parser[List[Instruction]] = instruction.*
 }
